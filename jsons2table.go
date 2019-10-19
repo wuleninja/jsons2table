37220c4a702198680d1635634e21d14c9f76693c
ninjawule <24457421+ninjawule@users.noreply.github.com>
2019-10-19 23:30:00 +0200

    feat: controlling the folder path

--- jsons2table.go
+++ jsons2table.go
@@ -2,8 +2,27 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Printf("Hello %s", "World!")
+
+	// getting the args
+	args := os.Args[1:]
+	if len(args) == 0 {
+		err("Missing the folder name here! Please provide a path to a valid directory")
+	}
+
+	// getting the folder path, which should be a valid directory
+	folderPath := args[0]
+	if _, errPath := os.Stat(folderPath); os.IsNotExist(errPath) {
+		err("'%s' is not a valid directory!", folderPath)
+	}
+
+	fmt.Printf("Hello %s\n", "World!")
+}
+
+func err(strfmt string, args ...interface{}) {
+	fmt.Printf(strfmt+"\n", args...)
+	os.Exit(1)
 }
